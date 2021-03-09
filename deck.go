de3d8f067c03d15ccb047dc75ead08d5d8715fe7
farismecinovic <faris.mecinovic.np@gmail.com>
2021-03-09 23:21:34 +0100

    testing with go

--- deck.go
+++ deck.go
@@ -1,6 +1,11 @@
 package main
-
-import "fmt"
+import ("fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+)
 
 //Create a new type of 'deck', which is the slice of strings
 
@@ -29,4 +34,33 @@ func (d deck) print() {
 
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
+}
+
+func (d deck) shuffle()  {
+	source := rand.NewSource(time.Now().UnixNano()) //unixNano return the int64 that NewSource accepts
+	r := rand.New(source)
+	for i := range d {
+		newPosition := r.Intn(len(d) - 1)
+
+		d[i], d[newPosition] = d[newPosition],d[i]
+	}
+
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(fileName string) error  {
+	return ioutil.WriteFile(fileName, []byte(d.toString()), 0666 ) // 0666 - anyone can read and write this file
+}
+
+func newDeckFromFile(fileName string) deck {
+	bs, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+	s := strings.Split(string(bs), ",")
+	return deck(s)
 }
